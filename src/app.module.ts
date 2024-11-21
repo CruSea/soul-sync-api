e3915827f678df664699bb048fa068e6a1fd4364
biniyam-29 <bnmmarkos@gmail.com>
2024-11-22 00:51:44 +0300

    created resources with nest g resource message

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
