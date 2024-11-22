add18ff8efa02e72e160f66e701b26057f4c1fe6
biniyam-29 <bnmmarkos@gmail.com>
2024-11-22 17:43:29 +0300

    configured the create function inside messages.service.ts to handle message creation and also made Database Module global.

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class MessagesService {
-  create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+  constructor(private readonly databaseService: DatabaseService){}
+  
+  async create(createMessageDto: Prisma.MessageCreateInput) {
+    return this.databaseService.message.create({data: createMessageDto});
   }
 
   findAll() {
