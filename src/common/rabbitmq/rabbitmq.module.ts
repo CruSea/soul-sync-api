2a23bf62062adb634a7952e653e7475ffe5983a8
biniyam-29 <bnmmarkos@gmail.com>
2025-01-22 00:31:59 +0300

    made the database consumer available in the rabbitmq module.

--- src/common/rabbitmq/rabbitmq.module.ts
+++ src/common/rabbitmq/rabbitmq.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { RabbitmqService } from './rabbitmq.service';
 import { MessageExchangeService } from './message-exchange/message-exchange.service';
 import { ChatExchangeService } from './chat-exchange/chat-exchange.service';
+import { TelegramMessageValidator } from './consumers/database/message-validators/telegram-message-validator.service';
+import { DatabaseConsumerService } from './consumers/database/database-consumer.service';
 
 @Module({
-  providers: [RabbitmqService, MessageExchangeService, ChatExchangeService],
+  providers: [
+    RabbitmqService,
+    MessageExchangeService,
+    ChatExchangeService,
+    TelegramMessageValidator,
+    DatabaseConsumerService,
+  ],
   exports: [RabbitmqService, MessageExchangeService, ChatExchangeService],
 })
 export class RabbitmqModule {}
