294b79162cc00ac81a105b79fc24f5966ae0a9dc
biniyam-29 <bnmmarkos@gmail.com>
2025-01-22 23:42:48 +0300

    created an abstract class that can handle message consumption and things related to it and also created a dto to handle the data passed to it.

--- src/common/rabbitmq/consumers/dto/rabbitmq-consumer.dto.ts
+++ src/common/rabbitmq/consumers/dto/rabbitmq-consumer.dto.ts
@@ -2,8 +2,8 @@ import * as amqp from 'amqplib';
 import { IsString } from 'class-validator';
 
 export class RabbitMQConsumerDto {
-    @IsString()
-    queueName: string;
-    
-    channel: amqp.Channel;
+  @IsString()
+  queueName: string;
+
+  channel: amqp.Channel;
 }
