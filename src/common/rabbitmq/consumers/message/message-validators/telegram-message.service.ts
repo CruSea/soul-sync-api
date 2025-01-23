c77615e70dff0ad65cf73059522e81bd0cd56747
biniyam-29 <bnmmarkos@gmail.com>
2025-01-23 22:26:27 +0300

    fortmated telegram-message.service.ts with prettier.

--- src/common/rabbitmq/consumers/message/message-validators/telegram-message.service.ts
+++ src/common/rabbitmq/consumers/message/message-validators/telegram-message.service.ts
@@ -1,24 +1,30 @@
-import { Injectable } from "@nestjs/common";
-import { MessageTransmitterValidator } from "./message-validator";
-import { MessageDto, MessageType } from "../dto/message.dto";
+import { Injectable } from '@nestjs/common';
+import { MessageTransmitterValidator } from './message-validator';
+import { MessageDto, MessageType } from '../dto/message.dto';
 
 @Injectable()
-export class TelegramMessageValidatorService implements MessageTransmitterValidator {
-    supports(type: string): boolean {
-        return type === 'telegram';
+export class TelegramMessageValidatorService
+  implements MessageTransmitterValidator
+{
+  supports(type: string): boolean {
+    return type === 'telegram';
+  }
+  extractmenteeAddress(message: any): string | null {
+    if (
+      !message.payload.message ||
+      !message.payload.message.chat ||
+      !message.payload.message.text
+    ) {
+      return null;
     }
-    extractmenteeAddress(message: any): string | null {
-        if (!message.payload.message || !message.payload.message.chat || !message.payload.message.text) {
-            return null;
-        }
-        return message.payload.message.chat.id.toString();
-    }
-    processMessage(message: any, conversationId: string): MessageDto | null {
-        return {
-          body: message.payload.message.text,
-          conversationId,
-          createdAt: new Date().toISOString(),
-          type: MessageType.RECEIVED,
-        };
-    }
- }
+    return message.payload.message.chat.id.toString();
+  }
+  processMessage(message: any, conversationId: string): MessageDto | null {
+    return {
+      body: message.payload.message.text,
+      conversationId,
+      createdAt: new Date().toISOString(),
+      type: MessageType.RECEIVED,
+    };
+  }
+}
