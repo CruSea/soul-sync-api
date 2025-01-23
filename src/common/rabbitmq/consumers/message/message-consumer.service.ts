65afa22917252e737d57eb49b6e6fb2fed1b658c
biniyam-29 <bnmmarkos@gmail.com>
2025-01-23 10:11:39 +0300

    established a rabbit connection and configured the consumption by extending RabbitMQAbstractConsumer.

--- src/common/rabbitmq/consumers/message/message-consumer.service.ts
+++ src/common/rabbitmq/consumers/message/message-consumer.service.ts
@@ -1,10 +1,13 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import * as amqp from 'amqplib';
 import { RabbitMQConnectionService } from '../rabbit-connection.service';
+import { RabbitMQAbstractConsumer } from '../rabbitmq-abstract-consumer';
 
 @Injectable()
-export class MessageConsumerService {
-  private channel: amqp.Channel;
+export class MessageConsumerService
+  extends RabbitMQAbstractConsumer
+  implements OnModuleInit, OnModuleDestroy
+{
   private connection: amqp.Connection;
   private rabbitConnectionDetails: {
     queueName: 'message_queue';
@@ -12,21 +15,35 @@ export class MessageConsumerService {
     exchangeName: 'message';
     exchangeType: 'topic';
   };
-
   constructor(
     private readonly rabbitMQConnectionService: RabbitMQConnectionService,
-  ) {}
-
-  async OnModuleInit() {
+  ) {
+    super({ queueName: 'message_queue', channel: null });
+  }
+  async onModuleInit(): Promise<void> {
     try {
-        const { channel, connection } =
-      await this.rabbitMQConnectionService.createConnection(
-        this.rabbitConnectionDetails,
-      );
-    this.channel = channel;
-    this.connection = connection;
+      const { channel, connection } =
+        await this.rabbitMQConnectionService.createConnection(
+          this.rabbitConnectionDetails,
+        );
+      this.channel = channel;
+      this.connection = connection;
+      await this.consume();
     } catch (error) {
-        console.error('Error initializing MessageConsumerService:', error);
+      console.error('Error initializing MessageConsumerService:', error);
     }
+  }
+  async onModuleDestroy(): Promise<void> {
+    try {
+      await this.rabbitMQConnectionService.disconnect(
+        this.connection,
+        this.channel,
+      );
+    } catch (error) {
+      console.error('Error destroying MessageConsumerService:', error);
     }
+  }
+  handleMessage(message: any, msg: amqp.Message): Promise<void> {
+    return;
+  }
 }
