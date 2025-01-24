cd854158951df1a6c4d13ea9a6eff6a765c5a08a
biniyam-29 <bnmmarkos@gmail.com>
2025-01-24 15:40:47 +0300

    logic for sending message to telegram inside TelegramChatValidator.

--- src/common/rabbitmq/consumers/chat/chat-out-let-implementations/telegram-chat.service.ts
+++ src/common/rabbitmq/consumers/chat/chat-out-let-implementations/telegram-chat.service.ts
@@ -2,4 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { SendChatInterface } from './send-chat.interface';
 
 @Injectable()
-export class TelegramChatValidator {}
+export class TelegramChatValidator implements SendChatInterface {
+  support(): string {
+    return 'TELEGRAM';
+  }
+
+  async send(message: any): Promise<boolean> {
+    const telegramApiUrl = `https://api.telegram.org/bot${message.configuration.token}/sendMessage`;
+    const response = await fetch(telegramApiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        chat_id: message.payload.address,
+        text: message.payload.body,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error('Error sending message to Telegram:', errorResponse);
+      return false;
+    }
+
+    const telegramResponse = await response.json();
+    if (!telegramResponse.ok) {
+      console.error('Error sending message to Telegram:', telegramResponse);
+      return false;
+    }
+
+    console.log('Message sent to Telegram successfully:', telegramResponse);
+    return true;
+  }
+}
