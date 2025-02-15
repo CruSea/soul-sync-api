e4749ef4b4aea7ac331e537bffd34f158de7d53b
biniyam-29 <bnmmarkos@gmail.com>
2025-02-15 05:52:53 +0300

    first draft for the logic of the context class.

--- src/apps/consumers/database-consumer/module/database-consumer.service.ts
+++ src/apps/consumers/database-consumer/module/database-consumer.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { RmqContext } from '@nestjs/microservices';
 import { MessagePayload } from 'src/types/message';
+import { Strategy } from '../strategy/strategy';
+import { CreateMessageDto } from './dto/create-message.dto';
 
 @Injectable()
 export class DatabaseConsumerService {
-  async handleMessage(data: MessagePayload, context: RmqContext) {
-    console.log(data);
-    console.log(context);
-    const channel = context.getChannelRef();
-    const originalMsg = context.getMessage();
-    channel.ack(originalMsg);
-  }
+  private strategy: Strategy;
+  async handleMessage(data: MessagePayload, context: RmqContext) { }
+  async setStrategy(strategy: Strategy) { }
+  async saveToDatabase(data: CreateMessageDto) { }
 }
+
+// console.log(data);
+// console.log(context);
+// const channel = context.getChannelRef();
+// const originalMsg = context.getMessage();
+// channel.ack(originalMsg);
