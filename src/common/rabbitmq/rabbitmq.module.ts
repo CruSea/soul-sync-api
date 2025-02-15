ddb4d803343e612a1822c8444f0ac4deb8f5a99c
biniyam-29 <bnmmarkos@gmail.com>
2025-02-15 03:56:46 +0300

    added MessageExchangeQueuesService into dependency injection.

--- src/common/rabbitmq/rabbitmq.module.ts
+++ src/common/rabbitmq/rabbitmq.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { RabbitmqService } from './rabbitmq.service';
 import { MessageExchangeService } from './message-exchange/message-exchange.service';
 import { ChatExchangeService } from './chat-exchange/chat-exchange.service';
+import { MessageExchangeQueuesService } from './message-exchange/message-exchange-queues.service';
 
 @Module({
-  providers: [RabbitmqService, MessageExchangeService, ChatExchangeService],
+  providers: [
+    RabbitmqService,
+    MessageExchangeService,
+    ChatExchangeService,
+    MessageExchangeQueuesService,
+  ],
   exports: [RabbitmqService, MessageExchangeService, ChatExchangeService],
 })
 export class RabbitmqModule {}
