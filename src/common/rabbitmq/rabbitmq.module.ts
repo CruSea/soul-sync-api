cb6482074e2829f924e9a8f449460b530e85926b
biniyam-29 <bnmmarkos@gmail.com>
2025-02-15 04:10:13 +0300

    added the file path which I forgot to enter earlier.

--- src/common/rabbitmq/rabbitmq.module.ts
+++ src/common/rabbitmq/rabbitmq.module.ts
@@ -3,6 +3,7 @@ import { RabbitmqService } from './rabbitmq.service';
 import { MessageExchangeService } from './message-exchange/message-exchange.service';
 import { ChatExchangeService } from './chat-exchange/chat-exchange.service';
 import { MessageExchangeQueuesService } from './message-exchange/message-exchange-queues.service';
+import { ChatExchangeQueuesService } from './chat-exchange/chat-exchange-queues.service';
 
 @Module({
   providers: [
