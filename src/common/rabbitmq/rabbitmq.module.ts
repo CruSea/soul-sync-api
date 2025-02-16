2c13425b08df8063598b19b7700770089ae6d581
biniyam-29 <bnmmarkos@gmail.com>
2025-02-16 03:19:09 +0300

    registered prismaService as a provider inside rabbit module.

--- src/common/rabbitmq/rabbitmq.module.ts
+++ src/common/rabbitmq/rabbitmq.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RabbitmqService } from './rabbitmq.service';
 import { MessageExchangeService } from './message-exchange/message-exchange.service';
 import { ChatExchangeService } from './chat-exchange/chat-exchange.service';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
 
 @Module({
-  providers: [RabbitmqService, MessageExchangeService, ChatExchangeService],
+  providers: [RabbitmqService, MessageExchangeService, ChatExchangeService, PrismaService],
   exports: [RabbitmqService, MessageExchangeService, ChatExchangeService],
 })
 export class RabbitmqModule {}
