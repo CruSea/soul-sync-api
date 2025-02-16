ac142c6597a0ab49fdaaa44fb2db6c47abbf017d
biniyam-29 <bnmmarkos@gmail.com>
2025-02-16 23:09:20 +0300

    concrete-strategy logic.

--- src/apps/consumers/message-consumer/strategy/strategy.ts
+++ src/apps/consumers/message-consumer/strategy/strategy.ts
@@ -1,6 +1,6 @@
 import { SentMessageDto } from "../module/dto/sent-message.dto";
 
 export interface Strategy {
-  SupportChannelType(type: string): Promise<boolean>;
-  FormatIncomingMessage(payload: any): Promise<SentMessageDto>;
+  supportChannelType(type: string): boolean;
+  formatIncomingMessage(payload: any): Promise<SentMessageDto>;
 }
