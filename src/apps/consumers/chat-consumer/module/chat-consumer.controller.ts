d207185901b8dc326c938b589f22c2938e5eb072
biniyam-29 <bnmmarkos@gmail.com>
2025-02-16 17:46:44 +0300

    set to accept incoming message.

--- src/apps/consumers/chat-consumer/module/chat-consumer.controller.ts
+++ src/apps/consumers/chat-consumer/module/chat-consumer.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { EventPattern, Ctx, RmqContext, Payload } from '@nestjs/microservices';
+import { ChatConsumerService } from './chat-consumer.service';
+import { Chat } from 'src/types/chat';
 
 @Controller('chat-consumer')
-export class ChatConsumerController {}
+export class ChatConsumerController {
+  constructor(private readonly chatConsumerService: ChatConsumerService) {}
+  @EventPattern()
+  handleMessage(@Payload() data: Chat, @Ctx() context: RmqContext) {
+    this.chatConsumerService.handleMessage(data, context);
+  }
+}
--- src/apps/consumers/chat-consumer/module/chat-consumer.service.ts
+++ src/apps/consumers/chat-consumer/module/chat-consumer.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { RmqContext } from '@nestjs/microservices';
+import { Chat } from 'src/types/chat';
 
 @Injectable()
-export class ChatConsumerService {}
+export class ChatConsumerService {
+  handleMessage(data: Chat, context: RmqContext) {}
+}
