32f5d42bb75e03f43a0b5224960352aa224bf623
biniyam-29 <bnmmarkos@gmail.com>
2025-02-16 03:02:33 +0300

    fixed minor bugs on which data comes from which and allign with the data that is expected.

--- src/apps/consumers/database-consumer/concrete-strategies/telegram-strategy.service.ts
+++ src/apps/consumers/database-consumer/concrete-strategies/telegram-strategy.service.ts
@@ -6,15 +6,15 @@ import { Strategy } from '../strategy/strategy';
 @Injectable()
 export class TelegramStrategyService implements Strategy {
   async SupportChannelType(type: string): Promise<boolean> {
-    return type === 'telegram';
+    return type === 'TELEGRAM';
   }
 
   async FormatIncomingMessage(payload: any): Promise<CreateMessageDto> {
     return {
       type: MessageType.RECEIVED,
-      body: payload.message,
-      address: payload.userId,
-      channelId: payload.channelId,
+      body: payload.payload.message.text,
+      address: String(payload.payload.message.chat.id),
+      channelId: payload.metadata.channelId,
     };
   }
 }
