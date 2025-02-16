d065bccff9b709c04cc283fd5a5a5c92272f2f2c
biniyam-29 <bnmmarkos@gmail.com>
2025-02-16 19:04:06 +0300

    registered the concrete strategy.

--- src/apps/consumers/chat-consumer/module/chat-consumer.module.ts
+++ src/apps/consumers/chat-consumer/module/chat-consumer.module.ts
@@ -3,6 +3,8 @@ import { ChatConsumerController } from './chat-consumer.controller';
 import { ChatConsumerService } from './chat-consumer.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
+import { TelegramConcreteStrategyService } from '../concrete-strategies/telegram-concrete-strategy.service';
+import { PrismaService } from 'src/modules/prisma/prisma.service';
 
 @Module({
   imports: [
@@ -13,6 +15,12 @@ import { PrismaModule } from 'src/modules/prisma/prisma.module';
     PrismaModule,
   ],
   controllers: [ChatConsumerController],
-  providers: [ChatConsumerService],
+  providers: [
+    ChatConsumerService,
+    {
+      provide: 'chat-consumer-concrete-strategies',
+      useFactory: () => [new TelegramConcreteStrategyService(new PrismaService())],
+    },
+  ],
 })
 export class ChatConsumerModule {}
--- src/apps/consumers/chat-consumer/module/chat-consumer.service.ts
+++ src/apps/consumers/chat-consumer/module/chat-consumer.service.ts
@@ -1,11 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { RmqContext } from '@nestjs/microservices';
 import { Chat } from 'src/types/chat';
 import { Strategy } from '../strategy/strategy';
 
 @Injectable()
-export class ChatConsumerService {
+export class ChatConsumerService implements OnModuleInit {
   private strategy: Strategy;
+  constructor(
+    @Inject('chat-consumer-concrete-strategies')
+    private readonly concreteStrategies: Strategy[],
+  ) {}
+  async onModuleInit() {
+    console.log('concrete strategies: ', this.concreteStrategies);
+  }
   async handleMessage(data: Chat, context: RmqContext) {
     const channel = context.getChannelRef();
     const originalMessage = context.getMessage();
