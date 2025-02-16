1af07cc3c8edec1af5dcf92875c55b97b20d0636
biniyam-29 <bnmmarkos@gmail.com>
2025-02-16 03:10:57 +0300

    added try catches to avoid server crashes and improved the logic at some parts as well test it.

--- src/apps/consumers/database-consumer/module/database-consumer.service.ts
+++ src/apps/consumers/database-consumer/module/database-consumer.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable,  } from '@nestjs/common';
 import { RmqContext } from '@nestjs/microservices';
 import { MessagePayload } from 'src/types/message';
 import { Strategy } from '../strategy/strategy';
@@ -6,31 +6,46 @@ import { CreateMessageDto } from './dto/create-message.dto';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 
 @Injectable()
-export class DatabaseConsumerService {
+export class DatabaseConsumerService  {
   private strategy: Strategy;
   constructor(
-    @Inject('database-consumer-concrete-strategy')
+    @Inject('database-consumer-concrete-strategies')
     private readonly concreteStrategies: Strategy[],
     private readonly prisma: PrismaService,
-  ) {}
+  ) { }
+
   async handleMessage(data: MessagePayload, context: RmqContext) {
+    if (!context.getChannelRef() || !context.getMessage()) {
+      throw new Error('Channel or message is undefined');
+    }
+
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
     try {
       await this.setStrategy(data.metadata?.type);
+      if (!this.strategy) {
+        channel.nack(originalMsg);
+        throw new Error(`No strategy found for type ${data.metadata?.type}`);
+      }
       const message = await this.strategy.FormatIncomingMessage(data);
       await this.saveToDatabase(message);
       channel.ack(originalMsg);
     } catch (error) {
-      console.log('error saving the new message', error);
+      console.log('Error handling message', error);
       channel.nack(originalMsg);
     }
   }
+
   async setStrategy(type: string) {
-    this.strategy = await this.concreteStrategies.find((strategy) =>
-      strategy.SupportChannelType(type),
-    );
+    try {
+      this.strategy = await this.concreteStrategies.find((strategy) =>
+        strategy.SupportChannelType(type),
+      );
+    } catch (error) {
+      console.log('Error setting strategy', error);
+    }
   }
+
   async saveToDatabase(message: CreateMessageDto) {
     try {
       return this.prisma.$transaction(async (tx) => {
@@ -71,7 +86,8 @@ export class DatabaseConsumerService {
         });
       });
     } catch (error) {
-      console.log('error in db transaction', error);
+      console.log('Error saving to database', error);
+      throw error;
     }
   }
 }
