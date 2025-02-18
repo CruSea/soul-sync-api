ec10cae183df9bf3194a234fd7c99ddd996e286e
biniyam-29 <bnmmarkos@gmail.com>
2025-02-18 23:26:30 +0300

    centeralized queue creation and binding under message exchange.

--- src/common/rabbitmq/message-exchange/message-exchange.service.ts
+++ src/common/rabbitmq/message-exchange/message-exchange.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import * as amqp from 'amqplib';
+import { MessageExchangeQueuesService } from './message-exchange-queues.service';
 
 @Injectable()
 export class MessageExchangeService implements OnModuleInit, OnModuleDestroy {
@@ -10,6 +11,9 @@ export class MessageExchangeService implements OnModuleInit, OnModuleDestroy {
   private readonly EXCHANGE_NAME = 'message';
   private readonly EXCHANGE_TYPE = 'topic'; // (direct, fanout, topic)
 
+  constructor(
+    private readonly messageExchangeQueuesService: MessageExchangeQueuesService,
+  ) {}
   async onModuleInit() {
     await this.connect();
   }
@@ -24,6 +28,10 @@ export class MessageExchangeService implements OnModuleInit, OnModuleDestroy {
       durable: true,
     });
     console.log('MessageExchangeService Connected!');
+    await this.messageExchangeQueuesService.init(
+      this.channel,
+      this.EXCHANGE_NAME,
+    );
   }
 
   private async disconnect() {
