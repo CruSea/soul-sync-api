0bd248769032b57ebc2681a8cabb83f386c895ee
biniyam-29 <bnmmarkos@gmail.com>
2025-02-19 04:46:01 +0300

    docker set-up

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -3,7 +3,6 @@ import { ChatGateway } from './chat.gateway';
 import { RabbitmqModule } from 'src/common/rabbitmq/rabbitmq.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ChatService } from './chat.service';
-import { RedisModule } from 'src/common/redis/redis.module';
 import { SocketService } from './socket.service';
 
 @Module({
