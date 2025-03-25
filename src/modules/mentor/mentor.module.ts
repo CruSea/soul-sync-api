c5e78793383368d7e0804bba177d626e6050f74a
jerrygirmaa <jerihagbj@gmail.com>
2025-03-25 15:12:35 +0300

    Updated mentor.module.ts

--- src/modules/mentor/mentor.module.ts
+++ src/modules/mentor/mentor.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConversationModule } from './conversation/conversation.module';
+import { MentorProfileModule } from './profile/mentor-profile.module';
 
 @Module({
-  imports: [ConversationModule],
+  imports: [ConversationModule, MentorProfileModule],
 })
 export class MentorModule {}
