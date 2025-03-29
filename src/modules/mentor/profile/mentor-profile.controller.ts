11d4b980da20464fe87761dc1c35d913a53bd5df
jerrygirmaa <jerihagbj@gmail.com>
2025-03-29 20:59:16 +0300

    Updated MentorProfileController for account check

--- src/modules/mentor/profile/mentor-profile.controller.ts
+++ src/modules/mentor/profile/mentor-profile.controller.ts
@@ -6,21 +6,24 @@ import {
   UseGuards,
   ValidationPipe,
   Req,
+  Query,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/modules/auth/guard/auth/auth.guard';
 import { Roles } from 'src/modules/auth/auth.decorator';
 import { MentorProfileService } from './mentor-profile.service';
 import { UpdateMentorDto } from 'src/modules/admin/mentor/dto/update-mentor.dto';
+import { RoleGuard } from 'src/modules/auth/guard/role/role.guard';
+import { GetMentorDto } from 'src/modules/admin/mentor/dto/get-mentor.dto';
 
 @Controller('mentor')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @Roles('MENTOR')
 export class MentorProfileController {
   constructor(private readonly mentorService: MentorProfileService) {}
 
   @Get('profile')
-  async getProfile(@Req() req) {
-    return this.mentorService.getProfile(req.user.email);
+  async getProfile(@Req() req, @Query() getMentor: GetMentorDto) {
+    return this.mentorService.getProfile(req.user.email, getMentor);
   }
 
   @Patch('profile')
@@ -28,7 +31,12 @@ export class MentorProfileController {
     @Req() req,
     @Body(new ValidationPipe({ transform: true }))
     updateMentor: UpdateMentorDto,
+    @Query() getMentor: GetMentorDto,
   ) {
-    return this.mentorService.updateProfile(req.user.email, updateMentor);
+    return this.mentorService.updateProfile(
+      req.user.email,
+      updateMentor,
+      getMentor,
+    );
   }
 }
