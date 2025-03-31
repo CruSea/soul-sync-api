8976e9ba783c80f089472fbe5a4a361ae309c99f
Biniyam <bnmmarkos@gmail.com>
2025-04-01 00:00:42 +0300

    registered all dependencies

--- src/apps/moderator/moderator.module.ts
+++ src/apps/moderator/moderator.module.ts
@@ -3,10 +3,11 @@ import { ModeratorService } from './moderator.service';
 import { ModeratorController } from './moderator.controller';
 import { RabbitmqModule } from 'src/common/rabbitmq/rabbitmq.module';
 import { PrismaModule } from 'src/modules/prisma/prisma.module';
+import { EmbeddingService } from './embedding.service';
 
 @Module({
   imports: [PrismaModule, RabbitmqModule],
   controllers: [ModeratorController],
-  providers: [ModeratorService],
+  providers: [ModeratorService, EmbeddingService],
 })
 export class ModeratorModule {}
