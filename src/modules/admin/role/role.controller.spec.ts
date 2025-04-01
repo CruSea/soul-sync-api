7316949cb6dc19e072dd674ea2b7bbd9c37c50e5
bikilaketema <bikilaketema94@gmail.com>
2025-04-01 11:21:10 +0300

    refactor: rename RoleController to RolesController for consistency

--- src/modules/admin/role/role.controller.spec.ts
+++ src/modules/admin/role/role.controller.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { RoleController } from './role.controller';
+import { RolesController } from './role.controller';
 import { RoleService } from './role.service';
 
-describe('RoleController', () => {
-  let controller: RoleController;
+describe('RolesController', () => {
+  let controller: RolesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [RoleController],
+      controllers: [RolesController],
       providers: [RoleService],
     }).compile();
 
-    controller = module.get<RoleController>(RoleController);
+    controller = module.get<RolesController>(RolesController);
   });
 
   it('should be defined', () => {
