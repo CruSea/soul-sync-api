987fcb81ed7a146d813cde3c2d51c5df6cc6cf6c
bikilaketema <bikilaketema94@gmail.com>
2025-04-01 09:46:08 +0300

    changed adminProfile to profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { MentorModule } from './modules/mentor/mentor.module';
 import { ChatModule } from './modules/chat/chat.module';
 import { RabbitmqModule } from './common/rabbitmq/rabbitmq.module';
 import { MessageModule } from './modules/message/message.module';
-import { AdminProfileModule } from './modules/admin/adminProfile/admin-profile.module';
+import { ProfileModule } from './modules/admin/profile/profile.module';
 import { RoleModule } from './modules/admin/role/role.module';
 
 @Module({
@@ -27,7 +27,7 @@ import { RoleModule } from './modules/admin/role/role.module';
     ChatModule,
     RabbitmqModule,
     MessageModule,
-    AdminProfileModule,
+    ProfileModule,
     RoleModule,
   ],
   providers: [PrismaService],
