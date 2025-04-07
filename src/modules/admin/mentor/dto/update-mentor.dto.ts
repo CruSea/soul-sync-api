5f09b22be9318a7b2e7bc82ac40170524f8bef5b
jerrygirmaa <jerihagbj@gmail.com>
2025-04-07 12:01:07 +0300

    Update update-mentor.dto.ts with new optional fields

--- src/modules/admin/mentor/dto/update-mentor.dto.ts
+++ src/modules/admin/mentor/dto/update-mentor.dto.ts
@@ -1,20 +1,15 @@
 import {
-  IsEmail,
   IsOptional,
   IsString,
   IsInt,
-  IsBoolean,
   IsEnum,
   IsObject,
   Min,
+  IsEmail,
 } from 'class-validator';
 import { GenderType } from '@prisma/client';
 
 export class UpdateMentorDto {
-  @IsOptional()
-  @IsString()
-  name?: string;
-
   @IsOptional()
   @IsEmail()
   email?: string;
@@ -43,8 +38,4 @@ export class UpdateMentorDto {
   @IsOptional()
   @IsString()
   location?: string;
-
-  @IsOptional()
-  @IsBoolean()
-  isActive?: boolean;
 }
