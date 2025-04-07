383d239b9d6ca65433ce42728b9433983c7e47ee
jerrygirmaa <jerihagbj@gmail.com>
2025-04-07 17:16:50 +0300

    feat(account): updated the relative import inaccount.service

--- src/modules/admin/account/account.service.ts
+++ src/modules/admin/account/account.service.ts
@@ -2,10 +2,10 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
 import { REQUEST } from '@nestjs/core';
-import { PrismaService } from 'src/modules/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { RoleType } from '@prisma/client';
-import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { paginate } from 'src/common/helpers/pagination';
+import { PaginationDto } from '../../../common/dto/pagination.dto';
+import { paginate } from '../../../common/helpers/pagination';
 
 @Injectable()
 export class AccountService {
