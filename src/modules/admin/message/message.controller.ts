7c0d3cebb6a9854b86e0e8c52fe5329489801ecc
jerrygirmaa <jerihagbj@gmail.com>
2025-05-13 14:04:31 +0300

    remove relative path

--- src/modules/admin/message/message.controller.ts
+++ src/modules/admin/message/message.controller.ts
@@ -5,8 +5,8 @@ import {
   UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
-import { MessageService } from './message.service';
-import { AuthGuard } from '../../auth/guard/auth/auth.guard';
+import { MessageService } from 'src/modules/admin/message/message.service';
+import { AuthGuard } from 'src/modules/auth/guard/auth/auth.guard';
 
 @Controller('admin/messages')
 @UseGuards(AuthGuard)
