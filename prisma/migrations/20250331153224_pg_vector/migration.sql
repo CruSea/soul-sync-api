526a23864f8b0ab1f365cb6ea451234665f8a346
Biniyam <bnmmarkos@gmail.com>
2025-05-27 14:21:08 +0300

    removed migtation file to resolve conflict

--- prisma/migrations/20250331153224_pg_vector/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "vector";
-
--- AlterTable
-ALTER TABLE "Conversation" ADD COLUMN     "embedding" vector(768);
-
--- AlterTable
-ALTER TABLE "Mentor" ADD COLUMN     "embedding" vector(768);
